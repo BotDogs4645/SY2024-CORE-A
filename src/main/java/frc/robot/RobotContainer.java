e51fbfc2fad60e1263d24134953b2bb95cb7d991
Aidan747 <aidan747-8@outlook.com>
2024-01-11 14:33:32 -0600

    SWERVE TIME

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,21 +4,61 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.commands.*;
+import frc.robot.subsystems.*;
 
 /**
- * The robot container class. This houses the subsystems and a lot of the core
- * logic.
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
+
+  private final CommandXboxController driveController = new CommandXboxController(0);
+
+
+  private final Swerve drivetrain = new Swerve();
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-    configureBindings();
+    drivetrain.setDefaultCommand(
+        new TeleopSwerve(
+          drivetrain,
+          () -> -driveController.getLeftY(), // tanslation
+          () -> -driveController.getLeftX(), // strafe 
+          () -> -driveController.getRightX(), // rotation
+          () -> driveController.leftBumper().getAsBoolean() // feild oriented yes or no
+      )
+    );
+
+    configureButtonBindings();
   }
 
-  private void configureBindings() {}
+  /**
+   * Use this method to define your button->command mappings. Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {
+    driveController.a().onTrue(new InstantCommand(() -> {drivetrain.zeroGyro();}, drivetrain));
+  }
 
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
   public Command getAutonomousCommand() {
-    return Commands.print("No autonomous command configured");
+    // An ExampleCommand will run in autonomous
+    return new InstantCommand(() -> {});
   }
 }
