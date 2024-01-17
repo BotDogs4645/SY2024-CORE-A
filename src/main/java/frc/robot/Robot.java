fd9f46d6c2d87f575a064b1b472f77a850a7eab2
bsverdin <117467689+bsverdin@users.noreply.github.com>
2024-01-17 17:15:31 -0600

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,20 +5,33 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.Limelight;
 
 /**
- * The main robot class. This handles the robot container (which contains the
- * subsystems) and handles triggers during different modes.
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the
+ * name of this class or
+ * the package after creating this project, you must also update the
+ * build.gradle file in the
+ * project.
  */
 public class Robot extends TimedRobot {
+  private XboxController xbox = new XboxController(CONTROLLER);
+  private Limelight limelight = new Limelight();
+  private Arm arm = new Arm();
+  public static CTREConfigs ctreConfigs;
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
 
   @Override
   public void robotInit() {
+    ctreConfigs = new CTREConfigs();
     m_robotContainer = new RobotContainer();
   }
 
@@ -38,11 +51,6 @@ public class Robot extends TimedRobot {
 
   @Override
   public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
-    }
   }
 
   @Override
@@ -53,9 +61,6 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
-    }
   }
 
   @Override
