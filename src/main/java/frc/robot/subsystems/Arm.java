597943a796318b1a54c3b8b6c402428d1444021e
Conor Mika <mjoconnor110@gmail.com>
2024-01-17 15:49:02 -0600

    Create Arm.java

---
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -0,0 +1,25 @@
+package frc.robot.subsystems;
+
+import frc.robot.Constants;
+
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Arm extends SubsystemBase {
+    // create cancoder object
+    // create left talonfx
+    // create right talonfx
+  public Arm() {
+    
+  }
+
+
+  @Override
+  public void periodic() {
+    
+  }
+
+  @Override
+  public void simulationPeriodic() {}
+}
