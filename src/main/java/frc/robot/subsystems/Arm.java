e9ee5dfd8db7e7431fa29690038f8c09dc843d5a
bsverdin <117467689+bsverdin@users.noreply.github.com>
2024-01-24 16:20:08 -0600

    Fixed Arm.java (Still need PID control

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -1,55 +1,53 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.TalonFX;
-import com.ctre.phoenix.sensors.Cancoder;
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.TalonFX;
 
-import edu.wpi.first.wpilibj.drive.RobotDriveBase.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Arm extends SubsystemBase {
-
-    private TalonFX leftMotor;
-    private TalonFX rightMotor;
-    int degrees = _coder.getPosition();
-    leftMotor = new TalonFX(LEFT_MOTOR, MotorType.kBrushless);
-    rightMotor = new TalonFX(RIGHT_MOTOR, MotorType.kBrushless);
-    rightMotor.setInverted(true);
-
-
+    public TalonFX leftMotor;
+    public TalonFX rightMotor;
+    public CANcoder cancoder;
 
     public Arm() {
-    if (tid == 1) {
-        while (degrees < 45){
-            leftMotor.set(0.5);
-            rightMotor.set(0.5);
-        }
-        leftMotor.set(0);
-        rightMotor.set(0);
-    }
-    else if (tid == 2){
-        while (degrees < 90){
-            leftMotor.set(0.5);
-            rightMotor.set(0.5);
-        }
-        leftMotor.set(0);
-        rightMotor.set(0);
-    }
-    else{
-        leftMotor.set(0);
-        rightMotor.set(0);
+        leftMotor = new TalonFX(14);
+        rightMotor = new TalonFX(15);
+        cancoder = new CANcoder(16);
+        rightMotor.setInverted(true);
     }
 
-       
-
-}
-}
-
-  @Override
-  public void periodic() {
+    StatusSignal<Double> rotations;
+    double degrees; 
     
-  }
-  @Override
-  public void simulationPeriodic() {}
+
+    public void moveArm(int id) {
+        rotations = cancoder.getPosition();
+        degrees = rotations.getValue() * 360.0;
+        if (id == 1 && degrees < 45) {
+            leftMotor.set(0.4);
+            rightMotor.set(0.4);
+
+        }else if (id == 2 && degrees < 90) {
+            leftMotor.set(0.4);
+            rightMotor.set(0.4);
+        } else {
+            leftMotor.set(0);
+            rightMotor.set(0);
+            leftMotor.stopMotor();
+            rightMotor.stopMotor();
+         }
+
+    }
+
+    @Override
+    public void periodic() {
+
+    }
+
+    @Override
+    public void simulationPeriodic() {
+    }
+
 }
