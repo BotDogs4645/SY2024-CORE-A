faf22b87aadc096899240395a6f9a4be0524146b
Zach Buchar <kkzabuchar@gmail.com>
2024-01-25 17:17:08 -0600

    Update Launcher subsystem to use PID encoders (probably incorrect right now)

--- src/main/java/frc/robot/subsystems/Launcher.java
+++ src/main/java/frc/robot/subsystems/Launcher.java
@@ -1,19 +1,19 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.CANEncoder;
-import com.revrobotics.CANPIDController;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.SparkMaxPIDController;
-import com.revrobotics.SparkPIDController;
 import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkPIDController;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class Launcher extends SubsystemBase {
-    public CANSparkMax rightLaunchMotor, leftLaunchMotor;
-    public CANEncoder rightMotorEncoder, leftMotorEncoder;
-    public CANPIDController rightPIDController, leftPIDController;
-    public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput, maxRPM;
+    private CANSparkMax rightLaunchMotor, leftLaunchMotor;
+    private RelativeEncoder rightMotorEncoder, leftMotorEncoder;
+    private PIDController rightPIDController, leftPIDController;
+    private double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput, maxRPM;
 
     public Launcher() {
         leftLaunchMotor = new CANSparkMax(0, MotorType.kBrushless);
@@ -23,43 +23,34 @@ public class Launcher extends SubsystemBase {
         leftMotorEncoder = leftLaunchMotor.getEncoder();
         rightMotorEncoder = rightLaunchMotor.getEncoder();
 
-        leftPIDController = leftLaunchMotor.getPIDController();
-        rightPIDController = rightLaunchMotor.getPIDController();
-
-        leftPIDController.setFeedbackDevice(leftMotorEncoder);
-        rightPIDController.setFeedbackDevice(rightMotorEncoder);
-
         kP = 6e-5;
         kI = 0;
-        kD = 0;
         kIz = 0;
+        //implement kD
         kFF = 0.000015;
         kMaxOutput = 1;
         kMinOutput = -1;
 
-        leftPIDController.setP(kP);
-        leftPIDController.setI(kI);
-        leftPIDController.setD(kD);
-        leftPIDController.setIZone(kIz);
-        leftPIDController.setFF(kFF);
-        leftPIDController.setOutputRange(kMinOutput, kMaxOutput);
+        leftPIDController = new PIDController(kP, kI, kD);
+        rightPIDController = new PIDController(kP, kI, kD);
 
-        rightPIDController.setP(kP);
-        rightPIDController.setI(kI);
-        rightPIDController.setD(kD);
-        rightPIDController.setIZone(kIz);
-        rightPIDController.setFF(kFF);
-        rightPIDController.setOutputRange(kMinOutput, kMaxOutput);
-
-        double setPoint = 0.0;
-        rightPIDController.setReference(setPoint, CANSparkMax.ControlType.kVelocity);
-        leftPIDController.setReference(setPoint, CANSparkMax.ControlType.kVelocity);
     }
-      
 
-    public void launch() {
-        double maxRPM = 5676;
-    
-        rightLaunchMotor.set();
+    public void startLauncher(double desiredVelocity) {
+        // double percentOutput = desiredVelocity/ Constants.Swerve.maxSpeed;
+        // leftLaunchMotor.set(percentOutput);
+        // rightLaunchMotor.set(percentOutput);
+
+        rightLaunchMotor.set(rightPIDController.calculate(rightMotorEncoder.getVelocity(), desiredVelocity));
+        leftLaunchMotor.set(leftPIDController.calculate(leftMotorEncoder.getVelocity(), desiredVelocity));
+        
+    }
+
+    public double getLaunchVelocity(double verticalDistance, double horizontalDistance) {
+        double verticalVelocity = Math.sqrt(2 * Constants.Launcher.gravityAcceleration * verticalDistance);
+        double timeToTravel = (-1 * verticalVelocity + Math.sqrt(Math.pow(verticalVelocity, 2) + 2 * Constants.Launcher.gravityAcceleration * verticalDistance))/(Constants.Launcher.gravityAcceleration);
+        double horizontalVelocity = horizontalDistance/timeToTravel;
+        double launchVelocity = Math.sqrt(Math.pow(verticalVelocity, 2) + Math.pow(horizontalVelocity, 2));
+        return launchVelocity;
     }
 }
