2aff9a250c65bdf8cb1819c2c2d2d273a9443751
bsverdin <117467689+bsverdin@users.noreply.github.com>
2024-01-26 16:11:00 -0600

    Added ArmConstants to Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,6 +5,8 @@ import java.util.Map;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.util.Units;
 
 /**
  * The Constants class contains constant values. Generally, when there are magic
@@ -28,6 +30,43 @@ public class Constants {
         public static final double OFFSET_RIGHT = 0.32;
         public static final double OFFSET_UP = 0.55;
         public static final double OFFSET_FORWARDS = 0.0;
+    }
+    public static class ArmConstants {
+        public static enum PendulumCommand {
+            IdleWithPiece(-Math.PI / 4),
+            Idle(-Math.PI / 2),
+            Straight(0),
+            RetrieveAngle(Math.toRadians(-6)), //TODO: find retrieve angle
+            ;
+
+            private double angle;
+
+            private PendulumCommand(double position) {
+                this.angle = position;
+            }
+
+            public double get() {
+                return angle;
+            }
+            
+        }
+        /* State Space Settings */
+        public static double measurementDelay = 0.10339;
+
+        // Characterization
+        public static final double kS = 0.084424;
+        public static final double kV = 0.67768;
+        public static final double kA = 0.13784;
+
+        // Other
+        public static final double momentOfInertia = 5.2; // 17.43589
+        public static final double gearing = 100.0 / 1.0; // output / input
+        public static final TrapezoidProfile.Constraints pendulumConstraints = new TrapezoidProfile.Constraints(
+                Units.degreesToRadians(45),
+                Units.degreesToRadians(90)); // Max arm speed and acceleration.
+
+        public static final double heightOfAxis = Units.inchesToMeters(50.847); // from Drew
+        public static final double armLength = Units.inchesToMeters(45.011);
     }
 
     /**
