9bd848ce27ee1675f6b53cc83d0eac253a302d8b
Zach Buchar <kkzabuchar@gmail.com>
2024-02-05 16:16:21 -0600

    Refactor LauncherCommand to extend CommandBase and add note param

--- src/main/java/frc/robot/commands/LauncherCommand.java
+++ src/main/java/frc/robot/commands/LauncherCommand.java
@@ -1,24 +1,27 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.subsystems.Launcher;
 
-public class LauncherCommand {
+public class LauncherCommand extends CommandBase{
 
     private Launcher launcher;
-    public LauncherCommand (Launcher launcher){
+    private int noteId;
+
+    public LauncherCommand (Launcher launcher, int noteId){
         this.launcher = launcher;
+        this.noteId = noteId;
         addRequirements(launcher);
     }
     @Override
     public void initialize() {
-        int id;
         Commands.sequence(
             // Move arm up
             Commands.deadline(
                 Commands.waitSeconds(0.5),
-                Commands.run(() -> launcher.aimLauncher(id), launcher)
+                Commands.run(() -> launcher.aimLauncher(noteId), launcher)
             ),
             // Stop arm
             Commands.runOnce(() -> launcher.lockInAim(), launcher),
