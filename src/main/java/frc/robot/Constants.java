fdd73950669f4abb0af44df6933b6b8a1e37c866
Zach Buchar <kkzabuchar@gmail.com>
2024-02-05 16:47:24 -0600

    Add height constants for field elements

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -37,6 +37,12 @@ public final class Constants {
     public static final double kP = 0.05;
     public static final double kI = 0.0001;
     public static final double kD = 0.0;
+    public static final double ampHeight = 0.66;
+    public static final double speakerHeight = 1.984;
+    public static final double trapHeight = 1.569;
+    public static final double launcherHeight = 0.574;
+
+
   }
   public static final class Vision {
     public static final double kLimelightAngleDegrees = 55;
