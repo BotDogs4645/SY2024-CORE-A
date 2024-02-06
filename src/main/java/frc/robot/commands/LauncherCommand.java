b0156313e241efed66ea435fa6e0e82fa5d1a7e9
bsverdin <117467689+bsverdin@users.noreply.github.com>
2024-02-06 15:49:30 -0600

    Now uses Indexer in LauncherCommand.java

--- src/main/java/frc/robot/commands/LauncherCommand.java
+++ src/main/java/frc/robot/commands/LauncherCommand.java
@@ -4,34 +4,37 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.subsystems.Launcher;
-import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Indexer;
 
 public class LauncherCommand extends CommandBase{
 
     private Launcher launcher;
     private int tagId;
     private double desiredVelocity;
-    private Intake intake;
-    private boolean activated;
+    private Indexer indexer;
+    private double speed;
 
-    public LauncherCommand (Launcher launcher, Intake intake, int tagId, double desiredVelocity, boolean activated){
+    public LauncherCommand (Launcher launcher, Indexer indexer, int tagId, double desiredVelocity, double speed){
         this.launcher = launcher;
-        this.intake = intake;
+        this.indexer = indexer;
         this.tagId = tagId;
-        addRequirements(launcher, intake);
+        addRequirements(launcher, indexer);
     }
     @Override
     public void initialize() {
         Commands.sequence(
-            // Move launcher up
+            //Feed the note
             Commands.deadline(
                 Commands.waitSeconds(0.5),
-                Commands.run(() -> intake.setIntake(activated), intake),
+                Commands.run(() -> indexer.indexing(speed), indexer),
                 Commands.run(() -> launcher.startLauncher(desiredVelocity), launcher)
             ),
-
+            //Stop feeding and aim
             Commands.deadline(
                 Commands.waitSeconds(0.5),
+                Commands.run(() -> indexer.pleaseStop(), indexer),
+                Commands.run(() -> launcher.stopLauncher(), launcher),
+                Commands.waitSeconds(1),
                 Commands.run(() -> launcher.aimLauncher(tagId), launcher)
             ),
             // Stop arm
@@ -40,7 +43,7 @@ public class LauncherCommand extends CommandBase{
             // Wait a bit
             Commands.waitSeconds(0.1),
 
-            // Shoot balls
+            // Shoot notes
             Commands.deadline(
                 Commands.waitSeconds(1),
                 Commands.run(() -> launcher.launchNote(), launcher)
