9231039df3984206cc82aacce82344a7288fa73d
Zach Buchar <kkzabuchar@gmail.com>
2024-02-06 16:03:01 -0600

    Refactor Launcher class: reformat code and improve readability

--- src/main/java/frc/robot/subsystems/Launcher.java
+++ src/main/java/frc/robot/subsystems/Launcher.java
@@ -23,23 +23,24 @@ import frc.lib.util.LaunchCalculations;
 
 import frc.robot.Constants;
 import frc.robot.LimelightHelpers.LimelightResults;
+
 public class Launcher extends ProfiledPIDSubsystem {
-    private CANSparkMax rightLaunchMotor, leftLaunchMotor, aimLaunchMotor;
-    private RelativeEncoder rightMotorEncoder, leftMotorEncoder, aimMotorEncoder;
-    private PIDController rightPIDController, leftPIDController, aimPIDController;
-    //all the rotational PID stuff
+  private CANSparkMax rightLaunchMotor, leftLaunchMotor, aimLaunchMotor;
+  private RelativeEncoder rightMotorEncoder, leftMotorEncoder, aimMotorEncoder;
+  private PIDController rightPIDController, leftPIDController, aimPIDController;
+  // all the rotational PID stuff
 
-    private ArmFeedforward feedforward;
-    private double pidWant;
-    private double ffWant;
-    public double wantedAngle;
-    private ShuffleboardTab tab;
-    // more stuff for launch calculations
-    double vertDistance = 0;
-    double horizDistance = 0;
+  private ArmFeedforward feedforward;
+  private double pidWant;
+  private double ffWant;
+  public double wantedAngle;
+  private ShuffleboardTab tab;
+  // more stuff for launch calculations
+  double vertDistance = 0;
+  double horizDistance = 0;
 
-    public Launcher() {
-            super(
+  public Launcher() {
+    super(
         // The ProfiledPIDController used by the subsystem
         new ProfiledPIDController(
             8.25,
@@ -51,26 +52,25 @@ public class Launcher extends ProfiledPIDSubsystem {
     this.feedforward = new ArmFeedforward(0.48005, 0.54473, 1.3389, 0.19963);
     this.ffWant = 0;
     super.getController().setTolerance(1.5, .5);
-        leftLaunchMotor = new CANSparkMax(0, MotorType.kBrushless);
-        rightLaunchMotor = new CANSparkMax(0, MotorType.kBrushless);
-        aimLaunchMotor = new CANSparkMax(0, MotorType.kBrushless);
-        rightLaunchMotor.setInverted(true);
+    leftLaunchMotor = new CANSparkMax(0, MotorType.kBrushless);
+    rightLaunchMotor = new CANSparkMax(0, MotorType.kBrushless);
+    aimLaunchMotor = new CANSparkMax(0, MotorType.kBrushless);
+    rightLaunchMotor.setInverted(true);
 
-        leftMotorEncoder = leftLaunchMotor.getEncoder();
-        rightMotorEncoder = rightLaunchMotor.getEncoder();
-        aimMotorEncoder = aimLaunchMotor.getEncoder();
+    leftMotorEncoder = leftLaunchMotor.getEncoder();
+    rightMotorEncoder = rightLaunchMotor.getEncoder();
+    aimMotorEncoder = aimLaunchMotor.getEncoder();
 
-        leftPIDController = new PIDController(Constants.Launcher.kP, Constants.Launcher.kI, Constants.Launcher.kD);
-        rightPIDController = new PIDController(Constants.Launcher.kP, Constants.Launcher.kI, Constants.Launcher.kD);
-        aimPIDController = new PIDController(Constants.Launcher.kP, Constants.Launcher.kI, Constants.Launcher.kD);
+    leftPIDController = new PIDController(Constants.Launcher.kP, Constants.Launcher.kI, Constants.Launcher.kD);
+    rightPIDController = new PIDController(Constants.Launcher.kP, Constants.Launcher.kI, Constants.Launcher.kD);
+    aimPIDController = new PIDController(Constants.Launcher.kP, Constants.Launcher.kI, Constants.Launcher.kD);
 
-        this.tab = Shuffleboard.getTab("Launcher");
-        tab.addNumber("Launcher Angle (degrees)", () -> aimMotorEncoder.getPosition());
-        tab.addNumber("PID Want", () -> getPIDVoltage());
-        tab.addNumber("FF want", () -> getFFVoltage());
+    this.tab = Shuffleboard.getTab("Launcher");
+    tab.addNumber("Launcher Angle (degrees)", () -> aimMotorEncoder.getPosition());
+    tab.addNumber("PID Want", () -> getPIDVoltage());
+    tab.addNumber("FF want", () -> getFFVoltage());
 
-
-        tab.add(this);
+    tab.add(this);
   }
 
   @Override
@@ -87,9 +87,8 @@ public class Launcher extends ProfiledPIDSubsystem {
   }
 
   public double getAimPosition() {
-    return  aimMotorEncoder.getPosition() * (Math.PI / 180.0);
+    return aimMotorEncoder.getPosition() * (Math.PI / 180.0);
   }
-
 
   public double getError() {
     return super.getController().getPositionError();
@@ -107,58 +106,53 @@ public class Launcher extends ProfiledPIDSubsystem {
     this.wantedAngle = wantedAngle;
   }
 
-
   public double getFFVoltage() {
     return ffWant;
   }
 
-
-    public void startLauncher(double desiredVelocity) {
+  public void startLauncher(double desiredVelocity) {
     rightLaunchMotor.set(rightPIDController.calculate(rightMotorEncoder.getVelocity(), desiredVelocity));
     leftLaunchMotor.set(leftPIDController.calculate(leftMotorEncoder.getVelocity(), desiredVelocity));
-    
-   }
 
-    public void aimLauncher(int id){
-        if (id == 6 || id == 5){
-            vertDistance = Constants.Launcher.ampHeight - Constants.Launcher.launcherHeight;
-            horizDistance = AprilTag.getDirectDistance();
-        }
-        else if (id == 7 || id == 4){
-            vertDistance = Constants.Launcher.speakerHeight - Constants.Launcher.launcherHeight;
-            horizDistance = AprilTag.getDirectDistance();
-        }
-        else if (id == 11 || id == 12 || id == 13 || id == 14 || id == 15 || id == 16){
-            vertDistance = Constants.Launcher.trapHeight - Constants.Launcher.launcherHeight;
-            horizDistance = AprilTag.getDirectDistance();
-        }
-        else{
-            vertDistance = 0;
-            horizDistance = AprilTag.getDirectDistance();
-        }
-        LaunchCalculations launchcalculations = new LaunchCalculations(vertDistance, horizDistance);
-        wantedAngle = launchcalculations.getLaunchAngle();
-        if (aimMotorEncoder.getPosition() < wantedAngle){
-            aimLaunchMotor.set(0.4);
-        }
-        else if (aimMotorEncoder.getPosition() > wantedAngle){
-            aimLaunchMotor.set(-0.4);
-            }
-            else {
-                aimLaunchMotor.set(0);
-            }
-        }
-    public void lockInAim(){
-      aimLaunchMotor.set(0);
+  }
+
+  public void aimLauncher(int id) {
+    if (id == 6 || id == 5) {
+      vertDistance = Constants.Launcher.ampHeight - Constants.Launcher.launcherHeight;
+      horizDistance = AprilTag.getDirectDistance();
+    } else if (id == 7 || id == 4) {
+      vertDistance = Constants.Launcher.speakerHeight - Constants.Launcher.launcherHeight;
+      horizDistance = AprilTag.getDirectDistance();
+    } else if (id == 11 || id == 12 || id == 13 || id == 14 || id == 15 || id == 16) {
+      vertDistance = Constants.Launcher.trapHeight - Constants.Launcher.launcherHeight;
+      horizDistance = AprilTag.getDirectDistance();
+    } else {
+      vertDistance = 0;
+      horizDistance = AprilTag.getDirectDistance();
     }
-    public void launchNote(){
-        LaunchCalculations launchcalculations = new LaunchCalculations(vertDistance, horizDistance);
-        leftLaunchMotor.set(launchcalculations.getLaunchVelocity());
-        rightLaunchMotor.set(launchcalculations.getLaunchVelocity());
-      }
-    public void stopLauncher(){
-          leftLaunchMotor.set(0);
-          rightLaunchMotor.set(0);
+    LaunchCalculations launchcalculations = new LaunchCalculations(vertDistance, horizDistance);
+    wantedAngle = launchcalculations.getLaunchAngle();
+    if (aimMotorEncoder.getPosition() < wantedAngle) {
+      aimLaunchMotor.set(0.4);
+    } else if (aimMotorEncoder.getPosition() > wantedAngle) {
+      aimLaunchMotor.set(-0.4);
+    } else {
+      aimLaunchMotor.set(0);
     }
   }
 
+  public void lockInAim() {
+    aimLaunchMotor.set(0);
+  }
+
+  public void launchNote() {
+    LaunchCalculations launchcalculations = new LaunchCalculations(vertDistance, horizDistance);
+    leftLaunchMotor.set(launchcalculations.getLaunchVelocity());
+    rightLaunchMotor.set(launchcalculations.getLaunchVelocity());
+  }
+
+  public void stopLauncher() {
+    leftLaunchMotor.set(0);
+    rightLaunchMotor.set(0);
+  }
+}
