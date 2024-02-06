8d5f89fe467a9dfac51b3b758f2b068fa6cb940d
bsverdin <117467689+bsverdin@users.noreply.github.com>
2024-02-06 15:38:29 -0600

    Added intake motors to Indexer.java

--- src/main/java/frc/robot/subsystems/Indexer.java
+++ src/main/java/frc/robot/subsystems/Indexer.java
@@ -2,34 +2,42 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.subsystems.Intake;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 
 
 public class Indexer extends SubsystemBase{
-private CANSparkMax leftLaunchMotor;
-private CANSparkMax rightLaunchMotor;
-//intake motors exist
+    private CANSparkMax firstIntakeMotor, secondIntakeMotor;
+    private PIDController firstPIDController, secondPIDController;
+    private RelativeEncoder firstMotorEncoder, secondMotorEncoder;
     public void indexer (){
-        leftLaunchMotor = new CANSparkMax(0, MotorType.kBrushless);
-        rightLaunchMotor = new CANSparkMax(0, MotorType.kBrushless);
-        rightLaunchMotor.setInverted(true);
-        //intake motors gain conciousness
+        firstIntakeMotor = new CANSparkMax(0, MotorType.kBrushless);
+        secondIntakeMotor = new CANSparkMax(0, MotorType.kBrushless);
+        secondIntakeMotor.setInverted(true);
+
+        firstPIDController = new PIDController(Constants.Intake.kP, Constants.Intake.kI, Constants.Intake.kD);
+        secondPIDController = new PIDController(Constants.Intake.kP, Constants.Intake.kI, Constants.Intake.kD);
+
+        firstMotorEncoder = firstIntakeMotor.getEncoder();
+        secondMotorEncoder = secondIntakeMotor.getEncoder();
 
       }
     
 
     public void indexing(double speed){
-        leftLaunchMotor.set(speed);
-        rightLaunchMotor.set(speed);
-        //intake motors go brrr
+
+        firstIntakeMotor.set(0.5);
+        secondIntakeMotor.set(0.5);
     }
 
     public void pleaseStop(){
-          leftLaunchMotor.set(0);
-          rightLaunchMotor.set(0);
-         //intake motors stop going brrr
+        firstIntakeMotor.set(0);
+        secondIntakeMotor.set(0);
     }
 }
