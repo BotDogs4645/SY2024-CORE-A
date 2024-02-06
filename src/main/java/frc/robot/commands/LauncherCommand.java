2f6c47f033fb77d91c592cf4e92702d5602da377
bsverdin <117467689+bsverdin@users.noreply.github.com>
2024-02-06 15:33:13 -0600

    Fixed LauncherCommand.java to now have intake

--- src/main/java/frc/robot/commands/LauncherCommand.java
+++ src/main/java/frc/robot/commands/LauncherCommand.java
@@ -4,21 +4,32 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.subsystems.Launcher;
+import frc.robot.subsystems.Intake;
 
 public class LauncherCommand extends CommandBase{
 
     private Launcher launcher;
     private int tagId;
+    private double desiredVelocity;
+    private Intake intake;
+    private boolean activated;
 
-    public LauncherCommand (Launcher launcher, int tagId){
+    public LauncherCommand (Launcher launcher, Intake intake, int tagId, double desiredVelocity, boolean activated){
         this.launcher = launcher;
+        this.intake = intake;
         this.tagId = tagId;
-        addRequirements(launcher);
+        addRequirements(launcher, intake);
     }
     @Override
     public void initialize() {
         Commands.sequence(
-            // Move arm up
+            // Move launcher up
+            Commands.deadline(
+                Commands.waitSeconds(0.5),
+                Commands.run(() -> intake.setIntake(activated), intake),
+                Commands.run(() -> launcher.startLauncher(desiredVelocity), launcher)
+            ),
+
             Commands.deadline(
                 Commands.waitSeconds(0.5),
                 Commands.run(() -> launcher.aimLauncher(tagId), launcher)
