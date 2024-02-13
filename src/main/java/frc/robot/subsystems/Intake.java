66a8b5851c1a82009439daefd94e60af5de941ab
Bianca Verdin <bsverdin@cps.edu>
2024-02-13 16:04:51 -0600

    Update Intake.java

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -5,15 +5,8 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
-import com.revrobotics.RelativeEncoder;
-import edu.wpi.first.math.controller.PIDController;
-
-import frc.robot.Constants;
-
 public class Intake extends SubsystemBase {
     private CANSparkMax firstIntakeMotor, secondIntakeMotor;
-    private PIDController firstPIDController, secondPIDController;
-    private RelativeEncoder firstMotorEncoder, secondMotorEncoder;
 
     private boolean robotIntakeActivated = false;
 
@@ -21,12 +14,6 @@ public class Intake extends SubsystemBase {
         firstIntakeMotor = new CANSparkMax(0, MotorType.kBrushless);
         secondIntakeMotor = new CANSparkMax(0, MotorType.kBrushless);
         secondIntakeMotor.setInverted(true);
-
-        firstPIDController = new PIDController(Constants.Intake.kP, Constants.Intake.kI, Constants.Intake.kD);
-        secondPIDController = new PIDController(Constants.Intake.kP, Constants.Intake.kI, Constants.Intake.kD);
-
-        firstMotorEncoder = firstIntakeMotor.getEncoder();
-        secondMotorEncoder = secondIntakeMotor.getEncoder();
     }
 
     public void toggleIntake() {
@@ -40,8 +27,8 @@ public class Intake extends SubsystemBase {
 
     public void dynamicIntakeExecution(boolean robotIntakeActivated) {
         if (robotIntakeActivated) {
-            firstIntakeMotor.set(firstPIDController.calculate(firstMotorEncoder.getVelocity(), Constants.Intake.intakeMotorVelocity));
-            secondIntakeMotor.set(secondPIDController.calculate(secondMotorEncoder.getVelocity(), Constants.Intake.intakeMotorVelocity));
+            firstIntakeMotor.set(0.5);
+            secondIntakeMotor.set(0.5);
         } else {
             firstIntakeMotor.set(0);
             secondIntakeMotor.set(0);
