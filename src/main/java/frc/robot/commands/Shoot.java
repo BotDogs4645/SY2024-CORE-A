311264975ac24972c4b489199671b593b2f8b71b
Bianca Verdin <bsverdin@cps.edu>
2024-02-27 17:07:27 -0600

    Started coding Shoot Command (need to put delays)

--- src/main/java/frc/robot/commands/Shoot.java
+++ src/main/java/frc/robot/commands/Shoot.java
@@ -5,24 +5,52 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.IntakeIndexer;
+import frc.robot.subsystems.Launcher;
+import frc.robot.subsystems.Limelight;
 
 public class Shoot extends Command {
   /** Creates a new Shoot. */
-  public Shoot() {
+  Launcher launcher;
+  IntakeIndexer intakeIndexer;
+  Limelight limelight;
+  public Shoot(Launcher launcher, IntakeIndexer intakeIndexer, Limelight limelight) {
     // Use addRequirements() here to declare subsystem dependencies.
+    this.launcher = launcher;
+    this.intakeIndexer = intakeIndexer;
+    this.limelight = limelight;
+    addRequirements(launcher, intakeIndexer, limelight);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    if (intakeIndexer.hasNote()){
+      intakeIndexer.runIntake(0.5);
+    }
+    else{
+      intakeIndexer.stopIntake();
+    }
+      intakeIndexer.runFeeder(0.5);
+      launcher.startLauncher(0.5);
+      //put delay here (without causing a scheduling error)
+      launcher.stopLauncher();
+      launcher.aimLauncher();
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    launcher.startLauncher(limelight.getLaunchVelocity().getAsDouble());
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    launcher.stopLauncher();
+    intakeIndexer.stopIntake();
+    intakeIndexer.stopFeeder();
+  }
 
   // Returns true when the command should end.
   @Override
