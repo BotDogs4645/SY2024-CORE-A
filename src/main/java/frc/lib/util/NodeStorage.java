7f28ce3396c4bc066f3a90929038c98751764310
Carver <gamemaster370.carver@gmail.com>
2024-02-28 16:27:15 -0600

    Created the 'NodeStorage' and 'NodaltaskExecution' classes, geared towards the detection, processing, and execution of tasks related to each 'node' on the playing field of FIRST Robotics' Crescendo competition.

--- src/main/java/frc/lib/util/NodeStorage.java
+++ src/main/java/frc/lib/util/NodeStorage.java
@@ -1,16 +1,31 @@
 package frc.lib.util;
 
+import com.sun.org.apache.bcel.internal.generic.NOP;
+
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 public class NodeStorage {
     
-    public class Node {
-      int nodeID;
-      Command command;
-      Translation2d position;
-      int radius;
+    public static class Node {
+      public int nodeID;
+      public SequentialCommandGroup command;
+      public Translation2d position;
+      public int radius;
     }
 
-    public static final Node[] nodes = {};
+    static Node initalizeNode(int nodeID, SequentialCommandGroup nodeCommand, Translation2d nodePosition, int nodeRadius) {
+      Node nodeInstance = new Node();
+
+      nodeInstance.nodeID = nodeID;
+      nodeInstance.command = nodeCommand;
+      nodeInstance.position = nodePosition;
+      nodeInstance.radius = nodeRadius;
+
+      return nodeInstance;
+    }
+
+    public static final Node[] nodes = {
+      initalizeNode(0, new SequentialCommandGroup(), new Translation2d(), 0)
+    };
 }
---
+++ src/main/java/frc/robot/commands/NodalTaskExecution.java
@@ -0,0 +1,68 @@
+package frc.robot.commands;
+
+import java.util.Optional;
+
+import javax.swing.text.html.Option;
+
+import com.sun.org.apache.bcel.internal.Const;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.lib.util.AprilTag;
+import frc.robot.Constants;
+import frc.robot.RobotContainer;
+import frc.lib.util.NodeStorage;
+
+public class NodalTaskExecution extends Command {
+
+    public RobotContainer robotContainerInstance;
+    public AprilTag aprilTagInstance;
+    public AdvanceToTarget advanceToTargetInstance;
+
+    public Optional<NodeStorage.Node> currentNode = Optional.empty();
+
+    public NodalTaskExecution(RobotContainer robotContainerInstance, AprilTag aprilTagInstance, AdvanceToTarget advanceToTargetInstance) {
+        this.robotContainerInstance = robotContainerInstance;
+        this.aprilTagInstance = aprilTagInstance;
+        this.advanceToTargetInstance = advanceToTargetInstance;
+    }
+    
+    public Optional<NodeStorage.Node> detectCurrentNode(Pose2d currentPosition) {
+        for (NodeStorage.Node currentNodePosition : NodeStorage.nodes) {
+            if (aprilTagInstance.getPlanarDistance(currentPosition.getTranslation(), new Translation2d(currentNodePosition.position.getX(), currentNodePosition.position.getY())).get() < currentNodePosition.radius) {
+                return Optional.of(currentNodePosition);
+            }
+        }
+
+        return Optional.empty();
+    }
+
+    @Override
+    public void initialize() {
+        currentNode = detectCurrentNode(robotContainerInstance.getField().getRobotPose());
+
+        if (currentNode.isPresent()) {
+
+            advanceToTargetInstance.specifyTarget(new Transform2d(new Translation2d(currentNode.get().position.getX(), currentNode.get().position.getY()), new Rotation2d(0, 0)));
+            advanceToTargetInstance.schedule();
+
+            currentNode.get().command.schedule();
+        }
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        currentNode.get().command.cancel();
+        currentNode = Optional.empty();
+    }
+
+    @Override 
+    public boolean isFinished() {
+        return currentNode.isEmpty() || currentNode.get().command.isFinished();
+    }
+}
