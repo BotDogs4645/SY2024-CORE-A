5af722c6b506c05c0ef6bcf9308ef5a55d663d75
jratke <jratke@cps.edu>
2024-02-29 12:41:06 -0600

    Add button to spit out note for intake.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -12,6 +12,8 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.commands.DriveToTag;
 import frc.robot.commands.IntakeCommand;
@@ -80,6 +82,11 @@ public class RobotContainer {
 
     driveController.leftBumper().toggleOnTrue(intakeCommand);
 
+    driveController.leftTrigger().onTrue(new SequentialCommandGroup(
+      new InstantCommand(() -> intakeIndexer.startSpittingNote()),
+      new WaitCommand(1),
+      new InstantCommand(() -> intakeIndexer.stop())
+    ));
   }
 
   public Command getAutonomousCommand() {
