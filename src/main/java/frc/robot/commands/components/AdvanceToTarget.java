66533f70edd74ae3f37965c7255e94a908af7756
CraivMan <119150909+CraivMan@users.noreply.github.com>
2024-03-17 14:25:01 +0000

    Decreased speed and acceleration within trajectories generated by the 'AdvanceToTarget' class.

--- src/main/java/frc/robot/commands/components/AdvanceToTarget.java
+++ src/main/java/frc/robot/commands/components/AdvanceToTarget.java
@@ -64,8 +64,8 @@ public class AdvanceToTarget extends Command {
 
         TrajectoryConfig trajectoryConfig =
             new TrajectoryConfig(
-                    Constants.Swerve.maxSpeed / 2,
-                    Constants.AutoConstants.kMaxAccelerationMetersPerSecondSquared / 2)
+                    Constants.Swerve.maxSpeed / 8,
+                    Constants.AutoConstants.kMaxAccelerationMetersPerSecondSquared / 8)
                 // Add kinematics to ensure max speed is actually obeyed
                 .setKinematics(Constants.Swerve.swerveKinematics);
     
