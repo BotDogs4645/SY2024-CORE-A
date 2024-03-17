66ebd3d5c84cf719ec56e08bb28a640840e2660c
CraivMan <119150909+CraivMan@users.noreply.github.com>
2024-03-17 01:53:25 +0000

    Added an implementation to the robot's codebase pertaining to the automatic alignment with a note detected by our 'BackLimelight' neural network.

--- src/main/java/frc/robot/commands/CommandBuilder.java
+++ src/main/java/frc/robot/commands/CommandBuilder.java
@@ -4,13 +4,19 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants;
+import frc.robot.subsystems.Swerve;
+import frc.robot.commands.components.AdvanceToTarget;
+import frc.robot.commands.components.AlignNote;
 import frc.robot.commands.components.IntakeNote;
 import frc.robot.commands.components.PrepareToShoot;
+import frc.robot.subsystems.BackLimelight;
 import frc.robot.subsystems.IntakeIndexer;
 import frc.robot.subsystems.Pneumatics;
 import frc.robot.subsystems.Shooter;
@@ -60,4 +66,19 @@ public class CommandBuilder {
             .andThen(new WaitCommand(1.5))
             .andThen(() -> {intakeIndexer.stop(); System.out.println("edge");}, intakeIndexer);
     } 
+
+    public static Command AdvanceToTarget(Swerve swervedrive, Field2d playingField, Transform2d targetPosition) {
+        return new AdvanceToTarget(swervedrive, playingField, targetPosition);
+    }
+
+    public static Command AlignNote(Swerve drivetrain, Field2d playingField, BackLimelight backLimelightInstance) {
+        return new AlignNote(drivetrain, playingField, backLimelightInstance);
+    }
+
+    public static Command AutomaticNoteIntake(IntakeIndexer intakeIndexer, Swerve drivetrain, Field2d playingField, BackLimelight backLimelightInstance) {
+        return Commands.parallel(
+            new AlignNote(drivetrain, playingField, backLimelightInstance),
+            new IntakeNote(intakeIndexer)
+        );
+    }
 }
