44f1f1253e88066bc29ab6397ac61d77efaef8e2
Aidan747 <aidan747-8@outlook.com>
2024-03-28 14:01:19 -0500

    Java spec 4.8.6: no happiness in commits

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -51,8 +51,6 @@ public class RobotContainer {
     NamedCommands.registerCommand("Shoot Amp", CommandBuilder.ShootAmp(intakeIndexer, shooter, pneumatics));
     NamedCommands.registerCommand("Intake Note", CommandBuilder.IntakeNote(intakeIndexer));
 
-    // hi :)
-
     drivetrain.setDefaultCommand(
         new TeleopSwerve(
             drivetrain,
